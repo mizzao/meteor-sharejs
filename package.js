6fbc4b3a1443aabb7e042e20c56d09cad3fe98ed
Andrew Mao <mizzao@gmail.com>
2013-06-24 17:51:33 -0400

    initial organization

---
+++ package.js
@@ -0,0 +1,9 @@
+Package.describe({
+    summary: "server (& client library) to allow concurrent editing of any kind of content"
+});
+
+Npm.depends({share: "0.6.2"});
+
+Package.on_use(function (api) {
+
+});
