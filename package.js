43a967fafb33039f72932b0766685b060ddea816
Andrew Mao <mizzao@gmail.com>
2014-09-24 11:06:07 -0400

    Update api.versionsFrom to use something after 0.9.2.1
    The following error showed up in @channikhabra's PR, which seems to be fixed in 0.9.2.1
    
    Could not resolve the specified constraints for this project:
    Error: conflict: routepolicy@1.0.0-cordova1 vs 1.0.1

--- package.js
+++ package.js
@@ -57,7 +57,7 @@ function getFilesFromFolder(packageName, folder){
 }
 
 Package.onUse(function (api) {
-  api.versionsFrom("0.9.2");
+  api.versionsFrom("0.9.2.2");
 
   var _ = Npm.require("underscore");
 
